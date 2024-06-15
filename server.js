71df5ce944ff1903b9f56e58e4ff5fd8ecc3ffc0
Azarias Galama <129897944+azariasabera@users.noreply.github.com>
2024-06-15 13:05:29 +0300

    Successfully installed express and send a JSON object back to the client with http://localhost:3000/hello

---
+++ server.js
@@ -0,0 +1,25 @@
+/* const http = require("http");
+
+http.createServer(function(req, res){
+    res.write("Hello, world!");
+    res.end();
+    console.log("Server running")
+}).listen(3000); */
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/hello", (req, res) => {
+    // send {message: "Hello, world!"} back to the client
+
+    res.json({msg: "Hello, world!"}); // the object maps string to string
+
+    // what is the difference between res.send and res.json?
+    // res.send sends a string back to the client while res.json ...
+    // ... sends a JSON object back to the client
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
